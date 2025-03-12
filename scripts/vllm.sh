0f4d6c51ad8e3b64d32dca4577f4f96e0fa522f0
toilaluan <tranthanhluan.nd@gmail.com>
2025-03-12 08:15:23 +0000

    update

--- scripts/vllm.sh
+++ scripts/vllm.sh
@@ -4,3 +4,4 @@ uv pip install vllm
 pm2 del vllm
 pm2 start --name vllm "vllm serve $MODEL_NAME --enable-prefix-caching --enable-chunked-prefill"
 . .venv/bin/activate
+update-env VLLM_CONFIG__MODEL_NAME $MODEL_NAME
